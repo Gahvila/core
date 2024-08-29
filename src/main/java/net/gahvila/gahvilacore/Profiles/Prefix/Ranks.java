283cd7605bcad7212ecacff46826d61a4b1acebf
DaJokni <56400974+DaJokni@users.noreply.github.com>
2024-08-29 18:30:09 +0300

    prefix rewörkkiä

--- src/main/java/net/gahvila/gahvilacore/Profiles/Prefix/Ranks.java
+++ src/main/java/net/gahvila/gahvilacore/Profiles/Prefix/Ranks.java
@@ -1,9 +1,27 @@
 package net.gahvila.gahvilacore.Profiles.Prefix;
 
 public enum Ranks {
-    DEFAULT,
-    VIP,
-    MVP,
-    PRO,
-    ADMIN
+    //default
+    DEFAULT(""),
+    //legacy ranks
+    VIP("VIP"),
+    MVP("MVP"),
+    PRO("Pro"),
+    OG("OG"),
+    //new ranks from cheapest to best
+    MOCHA("Mocha"),
+    LATTE("Latte"),
+    CAPPUCCINO("Cappuccino"),
+    CORTADO("Cortado"),
+    ESPRESSO("Espresso");
+
+    private final String displayName;
+
+    Ranks(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
